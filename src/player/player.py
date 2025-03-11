68d815dc79a5cdb0dbab019551bb99ccf59aafb2
BusterZero99 <desilvalaurence+github@gmail.com>
2025-03-11 13:17:22 +0000

    Fixed rotation

--- src/player/player.py
+++ src/player/player.py
@@ -11,10 +11,12 @@ class Player (pygame.sprite.Sprite):
         self.image = image
         self.rect = self.image.get_rect(topleft = vector2)
 
+        self.angle = 0
+
     def update(self):
         pass
 
-    def rotate(self, angle):#
+    def rotate(self, angle):
         self.angle += angle
         self.image = pygame.transform.rotate(self.original_image, self.angle)
         self.rect = self.image.get_rect(center = self.rect.center)
@@ -22,7 +24,7 @@ class Player (pygame.sprite.Sprite):
     '''def dieFunct(self):
         print(f"Player died at {self.rect}")'''
 
-def handlePlayerEvents(player, event):
+def handlePlayerEvents(self, player, event):
     if event.type == pygame.MOUSEBUTTONDOWN:
         if player.rect.collidepoint(event.pos):
             player.rotate(45)
