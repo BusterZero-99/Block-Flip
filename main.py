1c66993c0798b2bb02e44bbed441fcc62828852d
BusterZero99 <desilvalaurence+github@gmail.com>
2025-03-11 13:38:04 +0000

    Moved update loop

--- main.py
+++ main.py
@@ -48,19 +48,7 @@ class Main():
 
                 refreshBackground(self, event, drawBackground, BackgroundSprite)
 
-            self.window.fill(self.bg_colour)
-
-            self.background_group.empty()
-            self.background_group.add(self.bgsprite)
-            self.background_group.draw(self.window)
-            self.background_group.update()
-
-            self.player_group.draw(self.window)
-            self.player_group.update()
-
-            # Update window:
-            pygame.display.update()
-            pygame.time.Clock().tick(60)
+            updateWindow(self)
 
 if __name__ == "__main__":
     Main()
