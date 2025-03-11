3e2a4201fccc28d40f8a55cb583b2a98a509ac68
BusterZero99 <desilvalaurence+github@gmail.com>
2025-03-11 13:24:11 +0000

    Changed image name

--- src/scripts/background.py
+++ src/scripts/background.py
@@ -10,7 +10,7 @@ def drawBackground(self, BackgroundSprite):
     # Draw background image positions:
     for i in range(0, self.width, 64):
         for j in range(0, self.height, 64):
-            self.bgsprite.append(BackgroundSprite(self.background_image, (i, j),1))
+            self.bgsprite.append(BackgroundSprite(self.bg_img, (i, j),1))
             self.background_group.add(self.bgsprite[k])
             k += 1
             self.background_group.draw(self.window)
