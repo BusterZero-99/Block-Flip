db6f21b1356c850f859ccd14eee7eca5ae5284e3
BusterZero99 <desilvalaurence+github@gmail.com>
2025-03-15 15:50:30 +0000

    Added placeholder

--- src/scripts/ground.py
+++ src/scripts/ground.py
@@ -8,7 +8,7 @@ def drawGround(self, GroundSprite):
 
     # Draw ground tiles only on the bottom row:
     for i in range(0, self.width, self.tile_size):
-        img = self.icon
+        img = self.placeHolder
         self.gsprite.append(GroundSprite(img, (i, self.height - self.tile_size), 1))
         self.ground_group.add(self.gsprite[k])
         k += 1
