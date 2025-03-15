ffbba4c50d9c045c5b00e35cd1c9b7780a4c79b5
BusterZero99 <desilvalaurence+github@gmail.com>
2025-03-15 15:43:22 +0000

    Added ground

--- src/settings/defaultImports.py
+++ src/settings/defaultImports.py
@@ -5,6 +5,7 @@ from src.player.player import *
 from src.sprites.backgroundSprite import BackgroundSprite
 from src.sprites.textSprite import TextSprite
 from src.scripts.background import drawBackground
+from src.scripts.ground import drawGround
 from src.scripts.events import *
 import src.settings.defaultSettings as ds
 
