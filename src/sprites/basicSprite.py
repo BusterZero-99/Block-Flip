51941010e6d5aa649860e81133b33ef82ebcd5eb
BusterZero99 <desilvalaurence+github@gmail.com>
2025-03-16 14:36:56 +0000

    Update and rename backgroundSprite.py to basicSprite.py

--- src/sprites/basicSprite.py
+++ src/sprites/basicSprite.py
@@ -1,12 +1,12 @@
 import pygame
 
-class BackgroundSprite (pygame.sprite.Sprite):
+class BasicSprite (pygame.sprite.Sprite):
 	def __init__(self, image, vector2, spriteType: int):
 		pygame.sprite.Sprite.__init__(self)
 
 		self.vector2 = vector2
 
-		# Set Background Sprite's image and position using given values:
+		# Set Basic Sprite's image and position using given values:
 		self.image = image
 		self.rect = self.image.get_rect(topleft = vector2)
 
@@ -14,5 +14,5 @@ class BackgroundSprite (pygame.sprite.Sprite):
 		pass
 
 	def dieFunct(self):
-		print(f"BG Sprite died at {self.vector2}")
+		print(f"Sprite died at {self.vector2}")
 		self.kill()
