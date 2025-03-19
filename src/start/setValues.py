a19a7b8dcfd012b9c246d31254775d05a1990955
BusterZero99 <desilvalaurence+github@gmail.com>
2025-03-19 13:01:04 +0000

    Add files via upload

---
+++ src/start/setValues.py
@@ -0,0 +1,16 @@
+def setValues(self, ds):
+
+    self.width = ds.width
+    self.height = ds.height
+    self.window_name = ds.window_name
+    self.bg_colour = ds.bg_colour
+    self.tile_size = ds.tile_size
+
+    self.icon = ds.icon
+    self.player_img = ds.player_img
+    self.placeHolder = ds.placeHolder
+
+    self.uiImgs = ds.uiImgs
+    self.bgImgs = ds.bgImgs
+
+    self.font = ds.font
