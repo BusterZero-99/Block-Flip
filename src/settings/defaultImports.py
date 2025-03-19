616c3014382b67e76ebf17802ba449fe19d6abf1
BusterZero99 <desilvalaurence+github@gmail.com>
2025-03-19 13:04:05 +0000

    Update defaultImports.py

--- src/settings/defaultImports.py
+++ src/settings/defaultImports.py
@@ -1,12 +1,17 @@
 # Imports:
-import pygame, random
+import pygame
+
+import src.settings.defaultSettings as ds
+from src.start.setValues import setValues
 
 from src.player.player import *
 from src.sprites.basicSprite import BasicSprite
 from src.sprites.textSprite import TextSprite
-from src.scripts.background import drawBackground
-from src.scripts.ground import drawGround
+
+from src.start.background import drawBackground
+from src.start.ground import drawGround
+from src.start.ui import drawUi
+
 from src.scripts.events import *
-import src.settings.defaultSettings as ds
 
 from src.scripts.updateLoop import updateWindow
