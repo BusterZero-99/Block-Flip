e4cf8b7c59fb25ee02832d29e4209770568fbea3
BusterZero99 <desilvalaurence+github@gmail.com>
2025-03-26 14:01:46 +0000

    Update ground.py

--- src/start/ground.py
+++ src/start/ground.py
@@ -1,15 +1,13 @@
-import pygame
-
-def drawGround(self, GroundSprite):
+def drawGround(self, pygame, BasicSprite):
     # Create the ground:
     self.ground_group = pygame.sprite.Group()
     self.gsprite = []
     k = 0
+    img = self.placeHolder
 
     # Draw ground tiles only on the bottom row:
     for i in range(0, self.width, self.tile_size):
-        img = self.placeHolder
-        self.gsprite.append(GroundSprite(img, (i, self.height - self.tile_size), 1))
+        self.gsprite.append(BasicSprite(img, (i, self.height - self.tile_size), 1))
         self.ground_group.add(self.gsprite[k])
         k += 1
 
